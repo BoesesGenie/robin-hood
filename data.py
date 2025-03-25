c9cc5fe8e9aa94cc8e1280162483ff22b48bf3ce
boeses-genie <k.kharlamow@gmail.com>
2025-03-25 14:49:39 +0500

    model updated

--- data.py
+++ data.py
@@ -3,23 +3,48 @@ from model import Task
 _tasks: list[Task] = [
     Task(
         summary='Задача 1',
-        content='Содержимое задачи один.'
+        content='Содержимое задачи один.',
+        done=False,
+        start=0,
+        period=0,
+        created_at=0,
+        updated_at=0
     ),
     Task(
         summary='Задача 2',
-        content='Содержимое задачи два.'
+        content='Содержимое задачи два.',
+        done=False,
+        start=0,
+        period=0,
+        created_at=0,
+        updated_at=0
     ),
     Task(
         summary='Задача трёх тел',
-        content='Содержимое задачи трёх тел.'
+        content='Содержимое задачи трёх тел.',
+        done=False,
+        start=0,
+        period=0,
+        created_at=0,
+        updated_at=0
     ),
     Task(
         summary='Задача на сообразительность',
-        content='Содержимое задачи на сообразительность.'
+        content='Содержимое задачи на сообразительность.',
+        done=False,
+        start=0,
+        period=0,
+        created_at=0,
+        updated_at=0
     ),
     Task(
         summary='Задача на производительность',
-        content='Содержимое задачи на производительность.'
+        content='Содержимое задачи на производительность.',
+        done=False,
+        start=0,
+        period=0,
+        created_at=0,
+        updated_at=0
     )
 ]
 
--- model.py
+++ model.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class Task(BaseModel):
     summary: str
     content: str
+    done: bool
+    start: int
+    period: int
+    created_at: int
+    updated_at: int
